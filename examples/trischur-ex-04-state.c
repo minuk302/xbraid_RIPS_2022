d18347f36e343eac0f15d7f28d331e79aa73df33
minuk302 <minuk@DESKTOP-MOS39D0.localdomain>
2022-07-12 11:01:51 -0700

    fixed u flipping error and some calc error for v from u

--- examples/trischur-ex-04-state.c
+++ examples/trischur-ex-04-state.c
@@ -277,15 +277,14 @@ my_TriResidual(braid_App       app,
    }
    /* subtract rhs kbar (add (L^T D^{-T} V D^{-1}) g)*/
    if (index == 0)
-   {
-      
+   {      
       utmp[0] = 0.0;
       utmp[1] = -1.0;
       apply_Phi(dt, utmp);
       apply_Dinv(dt, utmp, utmp);
       apply_V(dt, gamma_1, gamma_2, utmp);
       apply_DAdjointinv(dt, utmp, utmp);
-      vec_axpy(2, 1.0, utmp, rtmp);
+      vec_axpy(2, -1.0, utmp, rtmp);
    }
    
    if (f!= NULL)
@@ -829,11 +828,11 @@ main(int argc, char *argv[])
                utmp[0] =  0.0;
                utmp[1] = -1.0;
                apply_Phi(dt, utmp);
-               vec_axpy(2, 1.0, utmp, v);
+               vec_axpy(2, -1.0, utmp, v);
             }
             apply_Dinv(dt, v, v);
             index = (app->ilower) + i + 1;
-            fprintf(file, "%05d: % 1.14e, %1.14e\n", index, v[0], v[1]);
+            fprintf(file, "%05d: % 1.14e, % 1.14e\n", index, v[0], v[1]);
          }
          vec_destroy(v);
          fflush(file);
